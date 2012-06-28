5e2c09dfb0c51547709961adf2968cb4ce57ce64
David Nuon <david@davidnuon.com>
2012-06-28 00:04:10 -0700

    Added filler tile.

--- app/views/parts/index/column_3.php
+++ app/views/parts/index/column_3.php
@@ -6,9 +6,9 @@ globe&rsquo;s largest computer science society. Here at the Long Beach chapter,
  
 <h2 class="col-post">ACM &hearts; GitHub</h2>
 <p>
-The Association for Computing Machinery is an active branch of the 
-globe&rsquo;s largest computer science society. Here at the Long Beach chapter,
- we believe in the principles of Gladwell&rsquo;s book, Outliers: The Story of Success. </p>
+        <img src="static/global/img/github.png" alt="" />
+    
+</p>
 
  <h2 class="col-post">Join ACM</h2>
 <p>
