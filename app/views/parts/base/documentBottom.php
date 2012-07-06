3727804e5ba0c5dc2adcf33e7d7363b5113ed5d9
davidnuon <david@davidnuon.com>
2012-07-06 11:16:20 -0700

    Worked on mobile, index looks okay:

--- app/views/parts/base/documentBottom.php
+++ app/views/parts/base/documentBottom.php
@@ -19,6 +19,34 @@
                 var last = location.pathname.split('/').length - 1;
                 var currentPage = location.pathname.split('/')[last];
                 $('#' + currentPage).addClass('select');
+
+                // Borrowed from Chris Coyier
+
+                $("<select />").appendTo("#nav");
+
+                $("<option />", {
+                   "selected": "selected",
+                   "value"   : "",
+                   "text"    : "Go to..."
+                }).appendTo("#nav select");
+              
+                $("<option />", {
+                     "value"   : "<?php echo SITEROOT; ?>",
+                     "text"    : "Home"
+                 }).appendTo("#nav select");
+
+                $("#nav a").each(function() {
+                 var el = $(this);
+                 $("<option />", {
+                     "value"   : el.attr("href"),
+                     "text"    : el.text()
+                 }).appendTo("#nav select");
+                });          
+
+                $("#nav select").change(function() {
+                  window.location = $(this).find("option:selected").val();
+                });
+
              })();            
         </script>
 	</body>
--- app/views/parts/base/header.php
+++ app/views/parts/base/header.php
@@ -1,6 +1,8 @@
 <div id="header" class="container_12">
     <div class="grid_6">
-        <h1><a href="<?php echo rx_siteURL(); ?>">
+        <h1>
+        <a href="<?php echo rx_siteURL(); ?>">
+          <span class="text-header">acm@thebeach</span>
         </a></h1>
     </div>
     
